452e4feb1f48fd476bd66f1fc3cf0e27aeae4c5d
Lightbringer <lighter0610@gmail.com>
2018-03-29 13:14:48 +0800

    1

--- Gold_Miner/place.h
+++ Gold_Miner/place.h
@@ -20,7 +20,7 @@
 #define BOTTON2PLACE 140		//按钮2纵坐标
 #define BOTTON3PLACE 260		//按钮3纵坐标
 ////////  按钮位置纵坐标
-#define TEXTMSGPLACEX FNPLACEX+110	//可变信息横坐标
+#define TEXTPLACEX FNPLACEX+110	//可变信息横坐标
 #define TEXT1PLACE 450			//信息1纵坐标
 #define TEXT2PLACE 500			//信息2纵坐标
 #define TEXT3PLACE 550			//信息3纵坐标
